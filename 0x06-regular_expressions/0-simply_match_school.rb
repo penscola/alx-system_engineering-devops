8b2093379155300f597490d8f069530adca80f12
penscola <penscolashackletonfelix@gmail.com>
2023-08-01 23:34:44 +0300

    chore: Simply matching School

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# The regular expression must match Holberton
+# Using the project instructions, create a Ruby script that accepts one argument and pass it to a regular expression matching method
+puts ARGV[0].scan(/Holberton/).join
