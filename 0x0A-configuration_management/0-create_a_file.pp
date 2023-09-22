0334c3bb30216102e796d682ece8887368bc17da
penscola <penscolashackletonfelix@gmail.com>
2023-09-22 21:14:36 +0300

    Install a package using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# Create a file
-file { '/tmp/holberton':
+# Create a file using puppet
+file { '/tmp/school':
   ensure  => file,
-  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
